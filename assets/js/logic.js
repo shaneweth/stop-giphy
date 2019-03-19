e8f457010ff8cd076ca3ea4c3ecaabd50c54eaac
willow <shaneweth@gmail.com>
2019-03-19 18:02:06 -0400

    Connected the API Key for GIPHY.com. While there is a response upon a query, there is an undefined object when the key values are targeted specifically

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,32 @@
+
+
+
 // Set Variables
 // ========================================
+// Not sure if these should be Global...
 
-const queryURL = "https:api.giphy.com/v1/gifs/" // Needs Query and API key
 let btnArr = ["Btn 1", "Btn 2", "Btn 3"]
+let giphy = $(this).attr("button");
+const apiKey = "&api_key=e0CoSKwRiwa2Bc1BlmjsuBomeLoxmCcs";
+const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giphy + apiKey; // Needs Query and API key
+
 // Build Functions
 // ========================================
+
+
+// on Click listener
+$("button").on("click", function () {
+
+    // AJAX Request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+        .then(function (response) {
+            console.log(response.data.embed_url); // What JSON data is pulled from the response []
+        });
+})
 
 // Create Processes
 // ========================================
@@ -13,27 +35,22 @@ let btnArr = ["Btn 1", "Btn 2", "Btn 3"]
 
 
 
-// AJAX Request
-$.ajax({
-    url: 'https://api.giphy.com/v1/gifs/trending?api_key=',
-}).then(function (response) {
-    console.log(response.data); // What JSON data is pulled from the response []
-});
+
 
 // function to create button element entered by user
 
 function renderButton() {
 
-    // Clear the #btn-div to prevent repeat buttons from being created in Array
-    $("#btn-div").empty();
+        // Clear the #btn-div to prevent repeat buttons from being created in Array
+        $("#btn-div").empty();
 
-    // Loop through array of gif search terms
-    for (let i = 0; i < btnArr.length; i++) {
+        // Loop through array of gif search terms
+        for (let i = 0; i < btnArr.length; i++) {
 
-        var b = $("<button>");
+            var b = $("<button>");
 
-        b.addClass("gify");
+            b.addClass("gify");
 
-        b.attr
+            b.attr
+        }
     }
-}
